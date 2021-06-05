d592643423a7c6199926758191a500e5d737f7f8
klazarenko@gmail.com <klazarenko@gmail.com>
2021-06-05 21:05:18 +0300

    updated prepare.sh and remove dependencies

--- prepare.sh
+++ prepare.sh
@@ -4,17 +4,18 @@ cp ../Packaging/resources/CharisSILB.ttf app/src/main/assets
 
 wget http://libsdl.org/release/SDL2-2.0.10.tar.gz
 tar -xzf SDL2-2.0.10.tar.gz
-rm SDL2-2.0.10/CMakeLists.txt
-mv SDL2-2.0.10/* SDL2
-rm -rf SDL2-2.0.10 SDL2-2.0.10.tar.gz
+mv SDL2-2.0.10 SDL2
+rm -rf SDL2-2.0.10.tar.gz
+cat sdl_cmakeoverwrite > SDL2/CMakeLists.txt
 
 wget https://www.libsdl.org/projects/SDL_mixer/release/SDL2_mixer-2.0.4.tar.gz
 tar -xzf SDL2_mixer-2.0.4.tar.gz
-mv SDL2_mixer-2.0.4/* SDL2_mixer
-rm -rf SDL2_mixer-2.0.4 SDL2_mixer-2.0.4.tar.gz
+mv SDL2_mixer-2.0.4 SDL2_mixer
+cat mix_cmakeoverwrite > SDL2_mixer/CMakeLists.txt
+rm -rf SDL2_mixer-2.0.4.tar.gz
 
 wget https://www.libsdl.org/projects/SDL_ttf/release/SDL2_ttf-2.0.15.tar.gz
 tar -xzf SDL2_ttf-2.0.15.tar.gz
-rm SDL2_ttf-2.0.15/CMakeLists.txt
-mv SDL2_ttf-2.0.15/* SDL2_ttf
-rm -rf SDL2_ttf-2.0.15 SDL2_ttf-2.0.15.tar.gz
+mv SDL2_ttf-2.0.15 SDL2_ttf
+rm -rf SDL2_ttf-2.0.15.tar.gz
+cat ttf_cmakeoverwrite > SDL2_ttf/CMakeLists.txt 
